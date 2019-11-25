0019e12d4ce8fe756d5e17966624ec9479c1f95e
Giscard123 <guiwene9979@gmail.com>
2019-11-25 18:00:53 +0100

    test modification

--- TestConsoleLibrary/Program.cs
+++ TestConsoleLibrary/Program.cs
@@ -9,7 +9,7 @@ using SellIt.Entities;
 using TestConsoleLibrary.Menus;
 using TestConsoleLibrary.Menus.Utils;
 
-namespace TestConsoleLibrary
+namespace TestConsoleLibrary //commentaire
 {
     public class Program
     {
