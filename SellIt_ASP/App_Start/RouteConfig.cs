d4a18a16c6ff8776ca6160a68ebcaffc6527594e
loic_simonetto <loic.simonetto@gmail.com>
2020-01-16 09:09:30 +0100

    route par default

--- SellIt_ASP/App_Start/RouteConfig.cs
+++ SellIt_ASP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SellIt_ASP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "IndexSellers", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
